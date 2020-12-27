086028683b2743361dbde16493dfc364554c0277
owenmccadden <52963762+owenmccadden@users.noreply.github.com>
2020-12-27 12:44:32 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package cards;
 
 public class Main {
 	public static void main(String[] args) {
-		Game g = new Game(1000);
+		Game g = new GameImpl(1000);
 		g.play();
 	}
 }
--- Player.java
+++ Player.java
@@ -1,8 +1,11 @@
 package cards;
 
 public interface Player {
-	
+	String getName();
+	int getChips();
+	PokerHand getHand();
 	void showCards();
-	
-	
+	void setHand(PokerHand p);
+	void placeBet(int bet);
+	void winHand(int pot);
 }
